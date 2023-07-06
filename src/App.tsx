bd32a8f3eb8c2ee705283255779a71394b5b594f
Vladimirs <vladimirribakov123@mail.ru>
2023-07-06 18:00:59 +0100

    created about project list and project pages and connected 'em to the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import About from './pages/About';
+import ProjectList from './pages/ProjectList';
+import ProjectPage from './pages/ProjectPage';
 import NotFound from './pages/NotFound';
 
 export function App() {
@@ -8,6 +11,9 @@ export function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<ProjectList />} />
+        <Route path="/projects/:projectId" element={<ProjectPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
