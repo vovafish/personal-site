ab84efc5aa8761dd5084dd8c0c0fb54dd38d3a1c
vovafish <vladimirribakov123@mail.ru>
2023-07-08 22:43:40 +0300

    created card stlye and dusplaying some data about projects

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useParams } from 'react-router-dom';
 
 import projects from '../projects';
@@ -14,13 +15,19 @@ function ProjectPage() {
   // object disctructuring
   // const { projectId } = params;
   return (
-    <>
-      <h1>{projectFound?.name}</h1>
-      <h2>{projectFound?.description}</h2>
+    <div className="p-5">
+      <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {projectFound?.name}
+        </h5>
+      </a>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {projectFound?.description}
+      </p>
       {projectFound?.stack.map((item) => (
         <p key={projectFound.link}>{item}</p>
       ))}
-    </>
+    </div>
   );
 }
 
