6688c83a8797d622487cfe4ee34099d0a4c01a43
vovafish <vladimirribakov123@mail.ru>
2023-07-11 21:17:45 +0300

    making upvotes option for projects

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,17 +1,35 @@
 import express, {Response, Request, Application} from "express"
 
+let projectsInfo = [{
+    link: 'project1',
+    upvotes: 0,
+}, {
+    link: 'project2',
+    upvotes: 0,
+}, {
+    link: 'project3',
+    upvotes: 0,
+}, {
+    link: 'project4',
+    upvotes: 0,
+}]
+
 const app: Application = express()
 // middleware
 app.use(express.json())
 
-app.post('/hello', (req: Request, res: Response) => {
-    console.log(req.body);
-    res.send('Hello');
+app.put('/api/projects/:link/upvote', (req, res) => {
+    const {link} = req.params;
+    const project  = projectsInfo.find(p => p.link === link)
+    if (project) {
+        project.upvotes += 1
+        res.send(`The ${link} project now has ${project.upvotes} upvotes`)
+    } else {
+        res.send(`That project doesn't exists`)
+    }
 })
 
 app.listen(8000, () => {
     console.log(`Listening on port 8000`);
     
 })
-
-console.log("Hi");
