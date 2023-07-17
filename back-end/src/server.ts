c6b449d632f8e43709d6a76cd4e75e0be1e64908
vovafish <vladimirribakov123@mail.ru>
2023-07-17 16:32:16 +0300

    adding login route to the server

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,108 +1,156 @@
-import express, { Response, Request, Application } from "express"
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { db, connectToDb } from "./db.js"
+import express, { Response, Request, Application } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { db, connectToDb } from './db.js';
 
-const app: Application = express()
+const app: Application = express();
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.get('/api/projects/:link', async (req, res) => {
-    const { link } = req.params;
+  const { link } = req.params;
 
-    const project = await db.collection('projects').findOne({ link });
+  const project = await db.collection('projects').findOne({ link });
 
-    if (project) {
-        res.json(project);
-    } else {
-        res.sendStatus(404);
-    }
-
-
-})
+  if (project) {
+    res.json(project);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 app.put('/api/projects/:link/upvote', async (req, res) => {
-    const { link } = req.params;
+  const { link } = req.params;
 
-    await db.collection('projects').updateOne({ link }, {
-        $inc: { upvotes: 1 }
-    });
-    const project = await db.collection('projects').findOne({ link })
-    if (project) {
-        res.json(project)
-    } else {
-        res.send(`That project doesn't exists`)
+  await db.collection('projects').updateOne(
+    { link },
+    {
+      $inc: { upvotes: 1 },
     }
-})
+  );
+  const project = await db.collection('projects').findOne({ link });
+  if (project) {
+    res.json(project);
+  } else {
+    res.send(`That project doesn't exists`);
+  }
+});
 
 app.post('/api/projects/:link/comments', async (req, res) => {
-    const { link } = req.params;
-    const { postedBy, text } = req.body;
+  const { link } = req.params;
+  const { postedBy, text } = req.body;
 
-
-    await db.collection('projects').updateOne({ link }, {
-        $push: { comments: { postedBy, text } },
-    });
-
-    const project = await db.collection('projects').findOne({ link })
-
-    if (project) {
-        res.json(project);
-    } else {
-        res.send(`That project doesn't exist`)
+  await db.collection('projects').updateOne(
+    { link },
+    {
+      $push: { comments: { postedBy, text } },
     }
+  );
+
+  const project = await db.collection('projects').findOne({ link });
+
+  if (project) {
+    res.json(project);
+  } else {
+    res.send(`That project doesn't exist`);
+  }
 });
 
 app.post('/api/register', async (req, res) => {
-    const { email, password, first_name, last_name, phone_number } = req.body;
+  const { email, password, first_name, last_name, phone_number } = req.body;
 
-    const user = await db.collection('users').findOne({ email })
+  const user = await db.collection('users').findOne({ email });
 
-    if (user) {
-        res.sendStatus(409); //conflict code error
-    }
+  if (user) {
+    res.sendStatus(409); // conflict code error
+  }
 
-    const passwordHash = await bcrypt.hash(password, 10);
+  const passwordHash = await bcrypt.hash(password, 10);
 
-    const result = await db.collection('users').insertOne({
-        email,
-        first_name,
-        last_name,
-        phone_number,
-        passwordHash,
-        isVerified: false,
-        isAdmin: false,
-    });
-    const { insertedId } = result;
+  const result = await db.collection('users').insertOne({
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    passwordHash,
+    isVerified: false,
+    isAdmin: false,
+  });
+  const { insertedId } = result;
 
-    jwt.sign({
-        id: insertedId,
-        email,
-        first_name,
-        last_name,
-        phone_number,
-        passwordHash,
-        isVerified: false,
-        isAdmin: false,
-
+  jwt.sign(
+    {
+      id: insertedId,
+      email,
+      first_name,
+      last_name,
+      phone_number,
+      passwordHash,
+      isVerified: false,
+      isAdmin: false,
     },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '2d',
-        },
-        (err, token) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            res.status(200).json({ token });
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '2d',
+    },
+    (err, token) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      res.status(200).json({ token });
+    }
+  );
+});
+
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await db.collection('users').findOne({ email });
+
+  if (!user) {
+    return res.sendStatus(401);
+  }
+
+  const {
+    _id: id,
+    isVerified,
+    isAdmin,
+    passwordHash,
+    first_name,
+    last_name,
+    phone_number,
+  } = user;
+
+  const isCorrect = await bcrypt.compare(password, passwordHash);
+
+  if (isCorrect) {
+    jwt.sign(
+      {
+        id,
+        isVerified,
+        isAdmin,
+        passwordHash,
+        first_name,
+        last_name,
+        phone_number,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '2d' },
+      (err, token) => {
+        if (err) {
+          res.status(500).json(err);
         }
-    )
-})
+
+        res.status(200).json({ token });
+      }
+    );
+  } else {
+    res.sendStatus(401);
+  }
+});
 
 connectToDb(() => {
-    console.log("Connected to the DB");
-    app.listen(8000, () => {
-        console.log(`Listening on port 8000`);
-
-    });
+  console.log('Connected to the DB');
+  app.listen(8000, () => {
+    console.log(`Listening on port 8000`);
+  });
 });
