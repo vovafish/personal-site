ec24698d6ce16f24117edef97d03e9fac6fe4639
vovafish <vladimirribakov123@mail.ru>
2023-07-27 12:30:49 +0300

    changed db name fix

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -5,7 +5,7 @@ let db;
 async function connectToDb(callback) {
     const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@personal-cluster.rkcohc2.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
-    db = client.db('my-presonal-projects'); // reference to the db
+    db = client.db('my-personal-projects'); // reference to the db
     callback();
 }
 
